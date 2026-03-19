10536b3f9d2584fd5673c0e83fc0644732e6fa00
GustavoSakuma <g.kuroiwa@aluno.ifsp.edu.br>
2026-03-18 21:23:51 -0300

    Add files via upload

---
+++ aula4ex1.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    float brinde = 0.0;
+    
+    printf ("valor da compra:\n "); 
+    scanf ("%f", &brinde);
+    
+    if ( brinde >= 50.00) {
+        printf ("VOCE GANHOU UM BRINDE!", brinde );
+    }
+
+
+    return 0;
+}
+
+
